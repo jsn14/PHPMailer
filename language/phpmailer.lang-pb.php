893d79e7a3dc00c06e14a67fc6d9de5d7cc34e5a
jsn14 <35091513+jsn14@users.noreply.github.com>
2019-11-12 17:48:09 -0500

    Create phpmailer.lang-pb.php

---
+++ language/phpmailer.lang-pb.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Punjabi (Gurmukhi script: pb_IN) PHPMailer language file: refer to English translation for definitive list
+ * @package PHPMailer 
+ * @author [Celebrating the 550th birth anniversary of Guru Nanak-the inventor of the `Gurmukhi Script`] (Jass: <jsn14@live.com>)
+ */
+$PHPMAILER_LANG['authenticate']         = 'SMTP ਨੁਕਸ: ਪ੍ਰਮਾਣਿਤ ਕਰਨ ਵਿੱਚ ਅਸਮਰੱਥ ਹੈ। ';
+$PHPMAILER_LANG['connect_host']         = 'SMTP ਨੁਕਸ: ਸਰਵਰ ਨਾਲ ਜੁੜਨ ਵਿੱਚ ਅਸਮਰੱਥ ਹੈ। ';
+$PHPMAILER_LANG['data_not_accepted']    = 'SMTP ਨੁਕਸ: ਡਾਟਾ ਸਵੀਕਾਰ ਕਰਨ ਵਿੱਚ ਅਸਮਰੱਥ ਹੈ। ';
+$PHPMAILER_LANG['empty_message']        = 'ਸੁਨੇਹਾ ਖਾਲੀ ਹੈ। ';
+$PHPMAILER_LANG['encoding']             = 'ਅਣਜਾਣ ਇੰਕੋਡਿੰਗ-ਕਿਸਮ';
+$PHPMAILER_LANG['execute']              = 'ਚਲਾਉਣ ਵਿੱਚ ਅਸਮਰੱਥ';
+$PHPMAILER_LANG['file_access']          = 'ਫਾਈਲ ਉਪਲਬਧ ਨਹੀਂ ਹੈ। ';
+$PHPMAILER_LANG['file_open']            = 'ਫਾਈਲ \'ਚ ਨੁਕਸ ਹੈ: ਫਾਈਲ ਨੂੰ ਖੋਲ੍ਹਿਆ ਨਹੀਂ ਜਾ ਸਕਿਆ। ';
+$PHPMAILER_LANG['from_failed']          = 'ਭੇਜਣ ਵਾਲੇ ਦਾ ਪਤਾ ਗਲਤ ਹੈ। ';
+$PHPMAILER_LANG['instantiate']          = 'ਮੇਲ ਫੰਕਸ਼ਨ ਨੂੰ ਕਾਲ ਨਹੀਂ ਕੀਤਾ ਜਾ ਸਕਿਆ।';
+$PHPMAILER_LANG['invalid_address']      = 'ਈਮੇਲ ਪਤਾ ਗਲਤ ਹੈ।';
+$PHPMAILER_LANG['mailer_not_supported'] = 'ਮੇਲ ਸਰਵਰ ਨਾਲ ਕੰਮ ਨਹੀਂ ਕਰਦਾ। ';
+$PHPMAILER_LANG['provide_address']      = 'ਕਿਰਪਾ ਕਰਕੇ ਘਟੋ-ਘੱਟ ਇੱਕ ਈ-ਮੇਲ ਪ੍ਰਾਪਤਕਰਤਾ ਦਾ ਪਤਾ ਦੇਵੋ। ';
+$PHPMAILER_LANG['recipients_failed']    = 'SMTP ਨੁਕਸ: ਇਹਨਾਂ ਪ੍ਰਾਪਤਕਰਤਾਵਾਂ ਨੂੰ ਭੇਜਣ ਵਿੱਚ ਅਸਮਰੱਥ। ';
+$PHPMAILER_LANG['signing']              = 'ਸਾਈਨ-ਇਨ ਨੁਕਸ:। ';
+$PHPMAILER_LANG['smtp_connect_failed']  = 'SMTP का connect () फ़ंक्शन विफल हुआ। ';
+$PHPMAILER_LANG['smtp_error']           = 'SMTP ਸਰਵਰ ਨੁਕਸ';
+$PHPMAILER_LANG['variable_set']         = 'ਵੇਰੀਏਬਲ ਬਣਾਏ ਜਾਂ ਸੰਸ਼ੋਧਿਤ ਨਹੀਂ ਕੀਤੇ ਜਾ ਸਕਦੇ।';
+$PHPMAILER_LANG['extension_missing']    = 'ਐਕਸਟੈਂਸ਼ਨ ਗੁੰਮ ਹੈ।';
